1466054c8c2efc5613c6c6efe3d473acfeb0f310
Emmanuel Omonzebaguan <codeplan73@gmail.com>
2023-08-10 12:55:10 +0100

    completed notification component

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const Notification = () => {
-  return <div>Notification</div>
+  return (
+    <div className="h-12 bg-red-500 text-white flex items-center justify-center text-center px-4 text-sm md:text-base cursor-pointer">
+      Free Delivery for all orders over $50. Order your food now!
+    </div>
+  )
 }
 
 export default Notification
